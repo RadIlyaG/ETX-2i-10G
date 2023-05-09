710f833aea7064e10fd842cc46c1ba3e176b8aba
Ilya Ginzburg <ilya_g@rad.com>
2023-05-09 11:51:57 +0300

    update HW init

--- AT-ETX-2i-10G/software/at-2i10g-2-w10/HWinit.tcl
+++ AT-ETX-2i-10G/software/at-2i10g-2-w10/HWinit.tcl
@@ -2,18 +2,18 @@
 set gaSet(javaLocation) C:\\Program\ Files\\Java\\jre1.8.0_181\\bin\\
 switch -exact -- $gaSet(pair) {
   1 - 5 - SE {
-      set gaSet(comDut)     4; #4 
-      set gaSet(com220)     10; #2 
-      set gaSet(comAux1)    6;  #9
-      set gaSet(comAux2)    7;  #7
+      set gaSet(comDut)     9
+      set gaSet(com220)     13
+      set gaSet(comAux1)    4
+      set gaSet(comAux2)    6
       set gaSet(comGpib)   13
       console eval {wm geometry . +150+1}
       console eval {wm title . "Con 1"} 
       set gaSet(pioBoxSerNum) FT2SUSS0  
   }
   2 {
-      set gaSet(comDut)      2; #6
-      set gaSet(com220)      5; #5
+      set gaSet(comDut)      8
+      set gaSet(com220)      10
       set gaSet(comAux1)   stam
       set gaSet(comAux2)   stam
       set gaSet(comGpib)   stam
