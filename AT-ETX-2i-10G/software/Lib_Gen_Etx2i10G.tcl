0bd2a8798240cef5bc3623ed9ae793d420abee25
Ilya Ginzburg <ilya_g@rad.com>
2024-06-02 11:18:44 +0300

    Support ETX-2I-10G-B_OFK/19/ACR/8SFPP (RetriveFansCheckJ)

--- AT-ETX-2i-10G/software/Lib_Gen_Etx2i10G.tcl
+++ AT-ETX-2i-10G/software/Lib_Gen_Etx2i10G.tcl
@@ -1658,7 +1658,9 @@ proc RetriveFansCheckJ {} {
       puts "if3.2"
       set fans 2
       set res [regexp {st\s+([\d\.\-]+)\s+([\d\.]+)\s+([0-9A-F]+)\s+([0-9A-F]+)\s+([0-9A-F]+)\s+} $buffer ma A B D F G]
-    } elseif {[string match *B.19.* $gaSet(DutInitName)] || [string match *B_TWC.19.* $gaSet(DutInitName)]} {
+    } elseif {[string match *B.19.* $gaSet(DutInitName)] || \
+              [string match *B_TWC.19.* $gaSet(DutInitName)] || \
+              [string match *B_OFK.19.* $gaSet(DutInitName)]} {
       puts "if4"
       set fans 2
       set res [regexp {st\s+([\d\.\-]+)\s+([\d\.]+)\s+([0-9A-F]+)\s+([0-9A-F]+)\s+([0-9A-F]+)\s+} $buffer ma A B D F G]
