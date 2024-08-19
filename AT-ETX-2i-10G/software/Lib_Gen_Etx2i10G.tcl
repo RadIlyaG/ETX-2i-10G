6cc0e778c2b2970e90553141c6c30d49fbe4c685
Ilya Ginzburg <ilya_g@rad.com>
2024-08-19 14:38:21 +0300

    Wait 5 minutes if ETX-2I-10G_SH/19/ACR/4SFPP/24SFP with SW 6.8.2(0.85) is testing (LY_wait)

--- AT-ETX-2i-10G/software/Lib_Gen_Etx2i10G.tcl
+++ AT-ETX-2i-10G/software/Lib_Gen_Etx2i10G.tcl
@@ -2462,7 +2462,8 @@ proc LY_wait {} {
     "ETX-2I-10G_LY.ACR.4SFPP.24SFP.tcl" "ETX-2I-10G_LY.DCR.12SFP12UTP.tcl" \
     "ETX-2I-10G_LY.DCR.24SFP.tcl" "ETX-2I-10G_LY.DCR.2SFPP.24SFP.tcl" \
     "ETX-2I-10G_LY.DCR.4SFPP.24SFP.tcl" "ETX-2I-10G_LY.H.DCR.4SFPP.24SFP.tcl"\
-    "ETX-2I-10G_MMC.ACR.4SFPP.24SFP.tcl" "ETX-2I-10G_SH.8.5.AC.4SFPP.8SFP.tcl"]
+    "ETX-2I-10G_MMC.ACR.4SFPP.24SFP.tcl" "ETX-2I-10G_SH.8.5.AC.4SFPP.8SFP.tcl"\
+    "ETX-2I-10G_SH.19.ACR.4SFPP.24SFP.tcl"]
     
   if {[lsearch $LY_wait_list $gaSet(DutInitName)]>"-1"} {
     set wait_option 1
