eeaaec066abb711be3aaad1e000d3b6608432f32
Ilya Ginzburg <ilya_g@rad.com>
2025-11-05 07:19:27 +0200

    Add Doron Elias as repairer; update Inits

---
+++ AT-ETX-2i-10G/software/doron-eli-hp-10/HWinit.tcl
@@ -0,0 +1,45 @@
+set gaSet(javaLocation) C:\\Program\ Files\\Java\\jre1.8.0_191\\bin
+switch -exact -- $gaSet(pair) {
+  1 - 5 - SE {
+      set gaSet(comDut)     4
+      set gaSet(com220)     5
+      set gaSet(comAux1)    NA
+      set gaSet(comAux2)    NA
+      set gaSet(comGpib)   NA
+      console eval {wm geometry . +150+1}
+      console eval {wm title . "Con 1"} 
+      set gaSet(pioBoxSerNum) FT81GTHPA
+  }
+  2 {
+      set gaSet(comDut)      5
+      set gaSet(com220)      6
+      set gaSet(comAux1)   NA
+      set gaSet(comAux2)   NA
+      set gaSet(comGpib)   NA
+      console eval {wm geometry . +150+200}
+      console eval {wm title . "Con 2"} 
+      set gaSet(pioBoxSerNum) FT6Z7YFA         
+  }
+  3 {
+      set gaSet(comDut)      7
+      set gaSet(com220)      8
+      set gaSet(comAux1)   NA
+      set gaSet(comAux2)   NA
+      set gaSet(comGpib)   NA
+      console eval {wm geometry . +150+200}
+      console eval {wm title . "Con 3"} 
+      set gaSet(pioBoxSerNum) FT6Z7YVW         
+  }
+  4 {
+      set gaSet(comDut)      9
+      set gaSet(com220)      10
+      set gaSet(comAux1)   NA
+      set gaSet(comAux2)   NA
+      set gaSet(comGpib)   NA
+      console eval {wm geometry . +150+200}
+      console eval {wm title . "Con 4"} 
+      set gaSet(pioBoxSerNum) FT6Z7YT7         
+  }
+  
+}  
+source lib_PackSour.tcl
--- AT-ETX-2i-10G/software/lib_PackSour.tcl
+++ AT-ETX-2i-10G/software/lib_PackSour.tcl
@@ -30,8 +30,8 @@ DeleteOldUserDef
 set host_name  [info host]
 if {[string match *avraham-bi* $host_name] || [string match *david-ya* $host_name] || \
     [string match *ofer-m-* $host_name] || [string match *shlomi-as* $host_name] || \
-    [string match *efraim-b* $host_name]} {
-  set ::repairMode 1
+    [string match *efraim-b* $host_name] || [string match *doron-eli* $host_name]} {
+  set ::repairMode 1 
 } else {
   set ::repairMode 0
 }
